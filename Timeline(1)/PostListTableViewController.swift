99ceac3b8e888342d69baa6fbd3dddec880fe360
rhand7 <handje@BC-MacBookPro-0587d1.local>
2017-04-17 13:44:55 -0600

    Initial Commit for Timeline

---
+++ Timeline(1)/PostListTableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PostListTableViewController.swift
+//  Timeline(1)
+//
+//  Created by handje on 4/17/17.
+//  Copyright © 2017 Rob Hand. All rights reserved.
+//
+
+import UIKit
+
+class PostListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - TableViewDataSource
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
